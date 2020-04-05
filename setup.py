f1a8b6ecb37e46ddf9cf6e5d63f853ca2310ee5b
Jason Milen <jason.milen@opengear.com>
2020-04-05 11:29:13 +1000

    add license file and update setup.py for registering package to test.pypi.org

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name='PyResourcePool',
-    version='0.1.0',
+    name='PyResourcePool-EjectedAlien',
+    version='0.1.1',
     author='Jason Milen',
     author_email='jpmilen@gmail.com',
-    packages=['pyresourcepool', 'pyresourcepool.test'],
     url='http://pypi.python.org/pypi/PyResourcePool/',
     license='LICENSE.txt',
     description='Provides a thread safe resource pool',
-    long_description=open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[],
     setup_requires=[
         'pytest-runner'
