b4289a73c539d8451fc09d814d8f71294ce86317
Jason Milen <jason.milen@opengear.com>
2020-05-12 14:23:55 +1000

    bump the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='PyResourcePool',
-    version='0.1.7',
+    version='0.1.8',
     author='Jason Milen',
     author_email='jpmilen@gmail.com',
     url='http://pypi.python.org/pypi/PyResourcePool/',
